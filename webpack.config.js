95b2b7a458211689490fccbd175f5d62dcff0c5d
Can Bartu Kılıç <91592857+cbartuk@users.noreply.github.com>
2022-10-30 13:23:15 +0300

    boiler-plate-multi

---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+const path = require("path");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+
+  mode: "development",
+
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+
+  optimization: {
+    minimize: false,
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+
+    new HtmlWebpackPlugin({
+      inject: "body",
+      title: "Anasayfa",
+      filename: "index.html", //output
+      template: "./src/views/home.html",  //input
+    }),
+
+    new HtmlWebpackPlugin({
+      inject: "body",
+      title: "Hakkimizda",
+      filename: "about.html", //output
+      template: "./src/views/about.html",  //input
+    }),
+
+    // [].concat(
+    //   pages.map(
+    //     (page) =>
+    //       new HtmlWebpackPlugin({
+    //         inject: "body",
+    //         title: `${page} Page || Kara Keçi`,
+    //         template: `./src/${page}.html`,
+    //       })
+    //   )
+    // ),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "/",
+            },
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.html$/,
+        loader: "underscore-template-loader",
+      },
+      // Images loader
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "./dist"),
+    },
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    open: true,
+    compress: true,
+    liveReload: true,
+    hot: true,
+    port: 9000,
+  },
+};
